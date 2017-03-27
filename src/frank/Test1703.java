2ca3d708a85387d5b107352b26ee915738025c2c
Mskanda <mskanda@student.tgm.ac.at>
2017-03-27 10:50:43 +0200

    Added 3 Times hallo

--- src/frank/Test1703.java
+++ src/frank/Test1703.java
@@ -3,5 +3,7 @@ package frank;
 public class Test1703 {
 	public static void main (String[] args){
 		System.out.println("Hallo Michael!");
+		System.out.println("Hallo Michael!");
+		System.out.println("Hallo Michael!");
 	}
 }
